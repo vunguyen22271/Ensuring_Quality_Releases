06acb117df10ffd214ec5c5838b6ee9bd06d0fae
vunguyen22271 <vunguyen22271@gmail.com>
2024-06-10 19:31:18 +0700

    fix: fix IaC pipeline

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,20 +1,26 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 # Start the browser and login with standard_user
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element(By.CSS_SELECTOR, "input[id='login-button']").click()
+    print('Login successful.')
 
 login('standard_user', 'secret_sauce')
 
 
-input("Press Enter to close the browser...")
+# input("Press Enter to close the browser...")
